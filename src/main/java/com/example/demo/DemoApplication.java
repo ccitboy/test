4e8aa1d44407d6ce03a8df2c479d98bc456b8125
cc <1558262258@qq.com>
2020-04-19 14:00:38 +0800

    测试

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("aaaaaa");
+		System.out.println("bbbb");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
